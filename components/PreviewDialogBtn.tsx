ab1e165d40b95d0fd19401e81b37358f48d39ea4
Aditi Kala <aditi25.kala@gmail.com>
2024-12-16 15:43:42 +0530

    Minor Update

--- components/PreviewDialogBtn.tsx
+++ components/PreviewDialogBtn.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Button } from './ui/button'
-import {MdOutlinePublish} from 'react-icons/md'
+import {MdPreview} from 'react-icons/md'
 
 export default function PreviewDialogBtn() {
   return (
     <Button variant={'outline'} className='gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400'>
-        <MdOutlinePublish className='h-4 w-4'/>
+        <MdPreview className='h-4 w-4'/>
         Publish
     </Button>
   )
