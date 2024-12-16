37df5e547d0bcafd10462a50d5c929c5b456009f
Aditi Kala <aditi25.kala@gmail.com>
2024-12-16 15:25:31 +0530

    Drag and drop

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -11,7 +11,7 @@ import { Button } from './ui/button';
 import { BiSolidTrash } from 'react-icons/bi';
 
 export default function Designer() {
-  const { elements, addElement, selectedElement, setSelectedElement } = useDesigner();
+  const { elements, addElement, selectedElement, setSelectedElement, removeElement } = useDesigner();
   const droppable = useDroppable({
     id: "designer-drop-area",
     data: {
@@ -25,17 +25,55 @@ export default function Designer() {
       if (!active || !over) return;
 
       const isDesignerBtnElement = active?.data?.current?.isDesignerBtnElement;
+      const isDroppingOverDesignerDropArea = over?.data?.current?.isDesignerDropArea;
 
-      if (isDesignerBtnElement) {
+      const droppingSidebarBtnOverDesignerDropArea = isDesignerBtnElement && isDroppingOverDesignerDropArea
+      if (droppingSidebarBtnOverDesignerDropArea) {
         const type = active?.data?.current?.type;
-        
-        if (type && FormElements[type as ElementsType]) {
-          const newElement = FormElements[type as ElementsType].construct(idGenerator());
-          
-          if (newElement) {
-            addElement(0, newElement);
-          }
+        const newElement = FormElements[type as ElementsType].construct(idGenerator());
+        addElement(elements.length, newElement);
+        return
+      }
+
+      const isDroppingOverDesignerELementTopHalf = over.data?.current?.isTopHalfDesignerElement
+      const isDroppingOverDesignerELementBottomHalf = over.data?.current?.isBottomHalfDesignerElement
+      const isDroppingOverDesignerELement = isDroppingOverDesignerELementTopHalf || isDroppingOverDesignerELementBottomHalf
+      const droppingSidebarBtnOverDesignerElement = isDesignerBtnElement && isDroppingOverDesignerELement
+
+      if(droppingSidebarBtnOverDesignerElement){
+        const type = active.data?.current?.type
+        const newElement = FormElements[type as ElementsType].construct(idGenerator())
+        const overId = over.data?.current?.elementId
+        const overElementIndex = elements.findIndex((el) => el.id === overId)
+        if(overElementIndex === -1){
+          throw new Error("Element not found")
         }
+        let indexForNewElement = overElementIndex
+        if(isDroppingOverDesignerELementBottomHalf){
+          indexForNewElement = overElementIndex + 1
+        }
+        addElement(indexForNewElement, newElement)
+        return
+      }
+
+      const isDraggingDesignerElement = active.data?.current?.isDesignerElement
+      const draggingDesignerElementOverAnotherDesignerElement = isDroppingOverDesignerELement && isDraggingDesignerElement
+
+      if(draggingDesignerElementOverAnotherDesignerElement){
+        const activeId = active.data?.current?.elementId
+        const overId = over.data?.current?.elementId
+        const activeElementIndex = elements.findIndex((el) => el.id === activeId)
+        const overElementIndex = elements.findIndex((el) => el.id === overId)
+        if(activeElementIndex === -1 || overElementIndex === -1){
+          throw new Error("Element not found")
+        }
+        const activeElement = {...elements[activeElementIndex]}
+        removeElement(activeId)
+        let indexForNewElement = overElementIndex
+        if(isDroppingOverDesignerELementBottomHalf){
+          indexForNewElement = overElementIndex + 1
+        }
+        addElement(indexForNewElement, activeElement)
       }
     },
   });
@@ -49,7 +87,7 @@ export default function Designer() {
           ref={droppable.setNodeRef}
           className={cn(
             "bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
-            droppable.isOver && "ring-2 ring-primary/20"
+            droppable.isOver && "ring-4 ring-primary ring-inset"
           )}
         >
           {!droppable.isOver && elements.length === 0 && (
