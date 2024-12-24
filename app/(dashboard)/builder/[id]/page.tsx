f2973b1fe8277c5abb8a253d0459f6b2c579b5db
Aditi Kala <aditi25.kala@gmail.com>
2024-12-24 11:23:48 +0530

    Revert "not done"
    This reverts commit b749ecfc5d7c90386d1c019c294c1280ae69ee99.

--- app/(dashboard)/builder/[id]/page.tsx
+++ app/(dashboard)/builder/[id]/page.tsx
@@ -1,22 +1,17 @@
-import { GetFormById } from '@/actions/form';
+import {GetFormById} from '@/actions/form';
 import FormBuilder from '@/components/FormBuilder';
 import React from 'react';
 
-interface BuilderPageProps {
-  params: {
-    id: string;
-  };
-}
+async function BuilderPage({ params }: { params: { id: string } }) {
+    const {id} = await params
+    const form = await GetFormById(Number(id)); 
+    if (!form){
+        throw new Error("Form not found")
+    }
 
-async function BuilderPage({ params }: BuilderPageProps) {
-  const { id } = params;
-  const form = await GetFormById(Number(id)); 
-  
-  if (!form) {
-    throw new Error("Form not found");
-  }
-
-  return <FormBuilder form={form} />;
+  return (
+    <FormBuilder form={form}/>
+  );
 }
 
 export default BuilderPage;
