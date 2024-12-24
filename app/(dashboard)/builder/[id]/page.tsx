b749ecfc5d7c90386d1c019c294c1280ae69ee99
Aditi Kala <aditi25.kala@gmail.com>
2024-12-24 10:34:44 +0530

    not done

--- app/(dashboard)/builder/[id]/page.tsx
+++ app/(dashboard)/builder/[id]/page.tsx
@@ -1,17 +1,22 @@
-import {GetFormById} from '@/actions/form';
+import { GetFormById } from '@/actions/form';
 import FormBuilder from '@/components/FormBuilder';
 import React from 'react';
 
-async function BuilderPage({ params }: { params: { id: string } }) {
-    const {id} = await params
-    const form = await GetFormById(Number(id)); 
-    if (!form){
-        throw new Error("Form not found")
-    }
+interface BuilderPageProps {
+  params: {
+    id: string;
+  };
+}
 
-  return (
-    <FormBuilder form={form}/>
-  );
+async function BuilderPage({ params }: BuilderPageProps) {
+  const { id } = params;
+  const form = await GetFormById(Number(id)); 
+  
+  if (!form) {
+    throw new Error("Form not found");
+  }
+
+  return <FormBuilder form={form} />;
 }
 
 export default BuilderPage;
